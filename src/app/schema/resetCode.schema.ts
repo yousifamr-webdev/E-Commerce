c6268c29c1a21f697bfe8827ccef220de5d6f373
yousifamr-webdev <yousifamr811@gmail.com>
2025-09-24 20:32:28 +0300

    sec commit

--- src/app/schema/resetCode.schema.ts
+++ src/app/schema/resetCode.schema.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const resetCodeSchema = z.object({
-  resetCode: z.string().min(6, "Code must be at least 6 digits")
+  resetCode: z.string().min(1, "Input can't be empty")
  
 });
 
